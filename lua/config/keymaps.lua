0c4fe91ce6e566b4271db3c6502b0731249936e6
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-11-19 14:56:38 -0300

    feat: keymap para poder formatear.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,5 @@ vim.keymap.set('n', '<C-h>', function() vim.cmd.wincmd({ args = { 'h' }}) end)
 vim.keymap.set('n', 'L', vim.cmd.bnext)
 vim.keymap.set('n', 'H', vim.cmd.bprev)
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format() end)

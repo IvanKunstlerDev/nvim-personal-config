ae8b28cfe4146b73b1d7b9e4fc60e694108592cb
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-11-19 14:55:03 -0300

    fix: evento lazy para git_signs al abrir un archivo

--- lua/plugins/git_signs.lua
+++ lua/plugins/git_signs.lua
@@ -1,10 +1,8 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  lazy = true,
-  config = function()
-    require('gitsigns').setup()
-  end,
-  opts = {
-    max_file_length = 5000,
-  },
+    'lewis6991/gitsigns.nvim',
+    event = 'BufReadPost',
+    config = true,
+    opts = {
+        max_file_length = 5000,
+    },
 }

3f58bd99f565500b85d7b08a419a3a960ae6a527
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-11-19 14:57:28 -0300

    refactor: configuraciones de indentacion movidas a autocmds.

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
+  end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python", "lua" },
+  callback = function()
+    vim.opt.tabstop = 4
+    vim.opt.shiftwidth = 4
+    vim.opt.expandtab = true
+  end
+})

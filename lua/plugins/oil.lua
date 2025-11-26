5ca07c4450cda0549f2b00a19302e8ae2f992400
IvanKunstlerDev <ivankunstlerdev@gmail.com>
2025-11-26 18:33:00 -0300

    fix: se agrego un keymap para cerra oil con q y se ocultaron las carpetas .. y .git

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,8 +3,15 @@ return {
 	---@module 'oil'
 	---@type oil.SetupOpts
 	opts = {
+		default_file_explorer = true,
 		view_options = {
 			show_hidden = true,
+			is_always_hidden = function(name, bufnr)
+				return name == ".." or name == ".git"
+			end,
+		},
+		keymaps = {
+			["q"] = "actions.close",
 		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
